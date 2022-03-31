31af48ea4b831fb945b54a50a40238ab7b774e0f
Brandon Robinson <brobinson71204@gmail.com>
2022-03-31 07:31:07 -0400

    Update

--- Database/database/history_desc.py
+++ Database/database/history_desc.py
@@ -1,3 +1,3 @@
-history_id=[]
-history_description=[]
-count=1
+history_id=['00000001', '00000002', '00000003', '00000004', '00000005', '00000006', '00000007']
+history_description=['admin tried to use a curse word known as: bitch', 'So I said hello back to him!', 'admin tried to use a curse word known as: bitch', 'So I said hello back to him!', 'Incorrect Password', 'admin tried to use a curse word known as: bitch', 'So I said hello back to him!']
+count=8
